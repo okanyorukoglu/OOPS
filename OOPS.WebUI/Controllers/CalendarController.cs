c03ca70aa0863f99b14368eb25f17b5477e2f55e
Okan Ozaman <okanozaman@gmail.com>
2020-06-09 19:55:32 +0300

    Takvim eklendi

---
+++ OOPS.WebUI/Controllers/CalendarController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OOPS.WebUI.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public IActionResult myCalendar()
+        {
+            return View("myCalendar");
+        }
+    }
+}
