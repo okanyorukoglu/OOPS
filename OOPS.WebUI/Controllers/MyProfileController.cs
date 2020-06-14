468ad29c5ed50ab2b1959a2caccdd786f27f6771
Ömer Kurtaran <omerkrtrn@hotmail.com>
2020-06-14 18:33:22 +0300

    navbar

--- OOPS.WebUI/Controllers/MyProfileController.cs
+++ OOPS.WebUI/Controllers/MyProfileController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OOPS.DTO.Employee;
+using OOPS.DTO.ProjectBase;
 
 namespace OOPS.WebUI.Controllers
 {
-    public class MyProfileController : Controller
+    public class MyProfileController : BaseController
     {
         public IActionResult Index()
         {
             return View();
         }
+
     }
+
+
 }
